122f4f9bc3f9f3caa085504c1914d499a00e2348
OlhaNester <olga_nv@ukr.net>
2021-03-22 22:48:40 +0200

    component

---
+++ src/component/MyFirstComp.jsx
@@ -0,0 +1,14 @@
+const MyFirstComp = () => {
+  return (
+    <div className="test-comp">
+      <h2> Test-Component</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio,
+        dolor quae. Nobis obcaecati minima molestias porro eveniet praesentium
+        dolore labore esse quam recusandae! Nihil, autem quia earum magni
+        maiores facere!
+      </p>
+    </div>
+  );
+};
+export default MyFirstComp;
