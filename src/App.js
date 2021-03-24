13c673a560bbf9196af4acc3d4e4e819acc65aed
OlhaNester <olga_nv@ukr.net>
2021-03-24 22:09:00 +0200

    222

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
-import logo from "./logo.svg";
-// import "./App.css";
-import MyFirstComp from "./component/MyFirstComp";
-import MySecondComp from "./component/MySecondComp";
+import Profile from "./components/Profile";
+import user from "./user.json";
 
-function App() {
-  const user = {
-    name: "Mango",
-  };
+function App({ user }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <MyFirstComp user={user} />
-      <MySecondComp user={user} />
-    </div>
+    <>
+      <Profile user={user} />
+    </>
   );
 }
 
